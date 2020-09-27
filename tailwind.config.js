c5b42098adf510608c5effcaa998c0df885a78d5
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-09-26 20:49:26 -0400

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue'
+    ],
+    options: {
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/
+      ]
+    }
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
